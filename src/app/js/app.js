9d5d86ec579c6d3edba625388b8816d208aa0568
sadhat <sadhat@hotmail.com>
2014-08-28 21:12:27 -0500

    ajax call added to json data

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -6,25 +6,3 @@ campusNextApp.config(['$routeProvider', function ($routeProvider) {
 		controller: 'HomeCtrl'
 	});
 }]);
-
-campusNextApp.controller('HomeCtrl', ['$scope', function ($scope) {
-    $scope.category = {
-        items: [
-            { title: "textbook search", totalItems:456 },
-            { title: "find tutor", totalItems: 50 },
-            { title: "room for rent", totalItems: 78 },
-            { title: "campus life events", totalItems: 10 },
-            { title: "share a ride", totalItems: 8 },
-            { title: "find study group", totalItems: 59 }
-        ]
-    };
-}]);
-
-campusNextApp.controller("CampusChooserCtrl", [
-    '$scope', function($scope) {
-        $scope.campus = {
-            name: "NDSU",
-            logoUrl: "NDSU.jpg"
-        };
-    }
-]);
