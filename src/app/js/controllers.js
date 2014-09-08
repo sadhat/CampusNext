e36391e4f939bae759432076bd7ece98ca691d8c
sadhat <sadhat@hotmail.com>
2014-09-08 16:34:46 -0500

    Controller added

---
+++ src/app/js/controllers.js
@@ -0,0 +1,19 @@
+﻿campusNextApp.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('src/app/data/categories/catagories.json').success(function (data) {
+        $scope.categories = data;
+    });
+}]);
+
+campusNextApp.controller("CampusChooserCtrl", ['$scope', '$http', function ($scope, $http) {
+    $http.get('src/app/data/campuses/viewing_campus.json').success(function (data) {
+        $scope.campus = data;
+    });
+}]);
+
+campusNextApp.controller("FindTextbookCtrl", ['$scope', '$http', function ($scope, $http) {
+    $scope.title = "find textbook";
+}]);
+
+campusNextApp.controller("TextbookSearchCtrl", ['$scope', '$http', function ($scope, $http) {
+    $scope.title = "Textbook Search";
+}]);
