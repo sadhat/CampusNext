36ef6906c2bbe6ea79ce3424203c9678dcecf5f6
sadhat <sadhat@hotmail.com>
2014-09-11 20:15:11 -0500

    Change folder structure

---
+++ database/scripts/tables/Textbook.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Textbook]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(250) NOT NULL, 
+    [ISBN] NVARCHAR(20) NULL, 
+    [Description] NVARCHAR(2000) NOT NULL, 
+    [Price] MONEY NULL, 
+    [CurrencyType] CHAR(4) NOT NULL DEFAULT 'us'
+)
