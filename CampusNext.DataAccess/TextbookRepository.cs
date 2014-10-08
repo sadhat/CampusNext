cb39b7e1cbe7a84f165b7c34e455f0cbe28a0f02
sadhat <sadhat@hotmail.com>
2014-10-07 19:48:02 -0500

    Data Access

---
+++ CampusNext.DataAccess/TextbookRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusNext.DataAccess
+{
+    public class TextbookRepository
+    {
+        public async Task<List<Textbook>> GetTextbooksAsync(int userId)
+        {
+            using (var context = new CampusNextContext())
+            {
+                var result = await context.Textbooks.Where((t) => t.UserId == userId).ToListAsync();
+                return result;
+            }
+        }
+
+        public async Task AddAsync(Textbook textbook)
+        {
+            using (var context = new CampusNextContext())
+            {
+                context.Entry(textbook).State = EntityState.Added;
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task SaveAsync(Textbook textbook)
+        {
+            using (var context = new CampusNextContext())
+            {
+                context.Entry(textbook).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(Textbook textbook)
+        {
+            using (var context = new CampusNextContext())
+            {
+                context.Entry(textbook).State = EntityState.Deleted;
+                await context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
