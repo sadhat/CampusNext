d0bdd1814ef601e4f28e481ac5cea28be9c47a4d
sadhat <sadhat@hotmail.com>
2014-10-22 19:14:48 -0500

    Change message for different campus

---
+++ web/src/app/js/directives/PreferredCampus.js
@@ -0,0 +1,16 @@
+﻿campusNextApp.directive("preferredcampus", [
+    "ProfileService", "CampusService", function (profileService, campusService) {
+        
+        return {
+            restrict: 'E',
+            link: function (scope, element, attrs) {
+                if (!profileService.profile) profileService.load();
+                var profile = profileService.profile;
+                if (campusService.getSelectedCampus().name !== profile.campusCode) {
+                    element.replaceWith('<div class="selectedCampusMessage">Your preferred campus is ' + profile.campusCode +
+                        '. Your post will go under this campus. If you want to change your preferred campus, please update from your profile.</div>');
+                }
+            }
+        }
+    }
+]);
