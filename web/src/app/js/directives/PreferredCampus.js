5f9ac6d9a731213dda734351b227fe704238585a
sadhat <sadhat@hotmail.com>
2014-10-22 19:21:38 -0500

    Refactor directive

--- web/src/app/js/directives/PreferredCampus.js
+++ web/src/app/js/directives/PreferredCampus.js
@@ -7,8 +7,12 @@
                 if (!profileService.profile) profileService.load();
                 var profile = profileService.profile;
                 if (campusService.getSelectedCampus().name !== profile.campusCode) {
-                    element.replaceWith('<div class="selectedCampusMessage">Your preferred campus is ' + profile.campusCode +
-                        '. Your post will go under this campus. If you want to change your preferred campus, please update from your profile.</div>');
+                    element.replaceWith('<div class="row"> <div class="medium-2 small-1 end columns">' +
+                        '</div> <div class="medium-10 small-11 columns"><label>' +
+                        '<div class="selectedCampusMessage">' +
+                        'Your preferred campus is ' + profile.campusCode +
+                        '. Your post will go under that campus. If you want to change your preferred campus, please update from your profile.</div>' +
+                        '</label></div></div>');
                 }
             }
         }
